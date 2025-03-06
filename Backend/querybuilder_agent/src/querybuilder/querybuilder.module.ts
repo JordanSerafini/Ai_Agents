495b8f7fe8f64592c7e08c78f5e64896a0e148f7
JordanS <jordanserafini.74@gmail.com>
2025-03-06 11:57:06 +0100

    working refacto

--- Backend/querybuilder_agent/src/querybuilder/querybuilder.module.ts
+++ Backend/querybuilder_agent/src/querybuilder/querybuilder.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { QueryBuilderService } from './services/query-builder.service';
 import { DatabaseMetadataService } from './services/database-metadata.service';
+import { DatabaseService } from './services/database.service';
 import { QueryBuilderController } from './controllers/querybuilder.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [QueryBuilderController],
-  providers: [QueryBuilderService, DatabaseMetadataService],
+  providers: [QueryBuilderService, DatabaseMetadataService, DatabaseService],
   exports: [QueryBuilderService, DatabaseMetadataService],
 })
 export class QueryBuilderModule {}
