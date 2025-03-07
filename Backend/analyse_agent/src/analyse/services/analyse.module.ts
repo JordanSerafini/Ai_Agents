fab6705affa5d935cf7dbb84300c891a81b894a5
JordanS <jordanserafini.74@gmail.com>
2025-03-07 16:47:28 +0100

    push

---
+++ Backend/analyse_agent/src/analyse/services/analyse.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { AnalyseService } from './analyse.service';
+import { OpenAIService } from './openai.service';
+import { RouterService } from './router.service';
+import {
+  QueryBuilderClientService,
+  ElasticsearchClientService,
+  RagClientLocalService,
+} from './clients';
+import {
+  CacheService,
+  ConversationService,
+  CategorizationService,
+  TemporalService,
+  FormatterService,
+  QueryAnalysisService,
+} from './analyse';
+
+@Module({
+  imports: [HttpModule, ConfigModule],
+  providers: [
+    AnalyseService,
+    OpenAIService,
+    RouterService,
+    QueryBuilderClientService,
+    ElasticsearchClientService,
+    RagClientLocalService,
+    CacheService,
+    ConversationService,
+    CategorizationService,
+    TemporalService,
+    FormatterService,
+    QueryAnalysisService,
+  ],
+  exports: [AnalyseService, OpenAIService, RouterService],
+})
+export class AnalyseModule {}
